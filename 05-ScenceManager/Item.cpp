bc91b76fc3bac74a989fadf84f2a17aef3dbb51c
NumberZeros <thotpbank050699@gmail.com>
2020-11-22 16:15:28 +0700

    update code item

--- 05-ScenceManager/Item.cpp
+++ 05-ScenceManager/Item.cpp
@@ -38,7 +38,12 @@ void CItem::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 	CGameObject::Update(dt);
 	vector<LPCOLLISIONEVENT> coEvents;
 	vector<LPCOLLISIONEVENT> coEventsResult;
+
+
 	coEvents.clear();
+
+	//cham dat
+	CalcPotentialCollisions(coObjects, coEvents);
 	CheckSize();
 
 	if (isFire == true)
@@ -57,6 +62,52 @@ void CItem::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 			y += speedy * dt;
 		}
 	}
+
+	if (coEvents.size() == 0)
+	{
+		//hb->UpdateHP(simon_HP);
+		x += dx;
+		y += dy;
+	}
+	else
+	{
+		float min_tx, min_ty, nx = 0, ny;
+		float rdx = 0;
+		float rdy = 0;
+		FilterCollision(coEvents, coEventsResult, min_tx, min_ty, nx, ny, rdx, rdy);
+		x += min_tx * dx + nx * 0.4f;
+		y += min_ty * dy + ny * 0.4f;
+
+
+		if (nx != 0) vx = 0;
+		if (ny != 0) vy = 0;
+
+		for (UINT i = 0; i < coEventsResult.size(); i++)
+		{
+			LPCOLLISIONEVENT e = coEventsResult[i];
+			if (dynamic_cast<CSimon*>(e->obj)) {
+				CSimon* simon = dynamic_cast<CSimon*>(e->obj);
+				if (isTorch || isFire || isCandle) {
+					if (nx != 0)
+						simon->x += simon->dx;
+					if (!simon->isGrounded) {
+						if (ny < 0)
+							simon->y += simon->dy;
+						if (ny > 0) {
+							simon->vy = -SIMON_JUMP_SPEED_Y;
+							simon->y += simon->dy;
+						}
+					}
+				}
+				else if (id == ITEM_ANI_CHAIN) {
+					simon->level += 1;
+					isHidden = true;
+					ResetBB();
+				}
+			}
+		}
+	}
+
 	for (UINT i = 0; i < coObjects->size(); i++)
 	{
 
